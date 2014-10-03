706abdc2b73df9c8cae5cd23a2c6f9382c98944a
Mohamed Ahmed <mohamed.ahmed@live.ca>
2014-10-03 13:21:49 -0400

    added Utils.js file and function to add course

--- client/js/main.js
+++ client/js/main.js
@@ -5,3 +5,12 @@ function getData(callback){
 		callback(data);
 	});
 }
+
+function addCourseToYear(yearNumber, courseName){
+	var yearObject = $( $(".year")[yearNumber-1] );
+
+	var courseObject = dom("div", {class:"course"}, document.createTextNode(courseName));
+
+	yearObject.append(courseObject);
+
+}
