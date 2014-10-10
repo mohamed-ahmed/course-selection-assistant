05269f7ed807a2585a1a5eeee98b172358b95777
Mohamed Ahmed <mohamed.ahmed@live.ca>
2014-10-10 19:10:50 -0400

    started login page

---
+++ server/login.php
@@ -0,0 +1,10 @@
+<?php
+
+	$login = $_POST['email'];
+	$firstname = $_POST['first-name'];
+	$lastname = $_POST['last-name'];
+	$password = $_POST['password'];
+
+	echo $login . $firstname . $lastname . $password
+
+?>
